10bdc1337b9fbbcd36412a794316ee92f1255e45
Djason Nathiez <d.nathiez@rubika-edu.com>
2022-12-13 17:24:21 +0100

    Debut SnakeGameCourse

---
+++ Sparky Engine/Sparky Engine/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+#define WIDTH 50
+#define HEIGHT 25
+
+using namespace std;
+
+int x = 10, y = 10;
+
+void board() 
+{
+	for (int i = 0; i < HEIGHT; i++)
+	{
+		cout << "\t\t#";
+
+		for (int j = 0; j < WIDTH; j++)
+		{
+			if (i == 0 || i == HEIGHT - 1) cout << '#';
+			else if (i == y && j == x) cout << '0';
+			else cout << ' ';
+		}
+
+		cout << "#\n";
+
+	}
+}
+
+int main() 
+{
+	board();
+}
